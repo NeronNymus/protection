52fe8c90f9636ffae116fc91acef55eec0bcda11
NeronNymus <neron.academico@gmail.com>
2024-12-11 15:25:53 -0600

    update

--- light/install_protection.ps1
+++ light/install_protection.ps1
@@ -100,9 +100,10 @@ if ($scheduledTask) {
 #$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'C:\Users\Public\Other\Schedule\run_schedule.ps1'
 #$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\Users\Beatriz Adriana G\Other\protection\light\run_protection.ps1"
 
+
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "`"C:\Users\Beatriz Adriana G\Other\protection\light\run_protection.ps1`""
 
-type "$scriptPath"
+type "$runPath"
 
 # Define triggers
 $t1 = New-ScheduledTaskTrigger -Daily -At 03:14pm
