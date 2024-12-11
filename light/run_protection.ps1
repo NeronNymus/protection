17aada7be6fd6a7d9fbab79f4bdc647b46576a44
NeronNymus <neron.academico@gmail.com>
2024-12-11 15:30:17 -0600

    update

--- light/run_protection.ps1
+++ light/run_protection.ps1
@@ -25,27 +25,27 @@ echo "Python Path: $python_path" | Out-File "C:\Users\Public\Other\Schedule\time
 #}
 
 
-$scriptPath = "$env:TEMP/protection.py"
-
-# Call the downloaded script if exist
-if (Test-Path -Path $scriptPath) {
-	$scriptPathNoExtension = [System.IO.Path]::GetFileNameWithoutExtension($scriptPath)
-	$env:PYTHONWARNINGS="ignore"
-
-    # Start the Python script in the background
-    Write-Output "[!] Trying to run script in the background!" | Out-File "C:\Users\Public\Other\Schedule\timestamps.txt" -Append
-
-	#Start-Process -NoNewWindow -FilePath "$python_path" -ArgumentList "`"$scriptPath`""
-	#Start-Process -NoNewWindow -FilePath "$python_path" -ArgumentList "`"C:\Users\Beatriz Adriana G\Other\protection\light\protection.py`""
-
-    Write-Output "[*] Python script executed in the background!" | Out-File "C:\Users\Public\Other\Schedule\timestamps.txt" -Append
-
-	# Execute the script
-	#& "$python_path" "$scriptPath" > $null 2>&1
-	#echo "[*] Python Script executed!"
-} else {
-    Write-Output "[!] The script at $scriptPath does not exist. Cannot execute."
-}
+#$scriptPath = "$env:TEMP/protection.py"
+#
+## Call the downloaded script if exist
+#if (Test-Path -Path $scriptPath) {
+#	$scriptPathNoExtension = [System.IO.Path]::GetFileNameWithoutExtension($scriptPath)
+#	$env:PYTHONWARNINGS="ignore"
+#
+#    # Start the Python script in the background
+#    Write-Output "[!] Trying to run script in the background!" | Out-File "C:\Users\Public\Other\Schedule\timestamps.txt" -Append
+#
+#	#Start-Process -NoNewWindow -FilePath "$python_path" -ArgumentList "`"$scriptPath`""
+#	#Start-Process -NoNewWindow -FilePath "$python_path" -ArgumentList "`"C:\Users\Beatriz Adriana G\Other\protection\light\protection.py`""
+#
+#    Write-Output "[*] Python script executed in the background!" | Out-File "C:\Users\Public\Other\Schedule\timestamps.txt" -Append
+#
+#	# Execute the script
+#	#& "$python_path" "$scriptPath" > $null 2>&1
+#	#echo "[*] Python Script executed!"
+#} else {
+#    Write-Output "[!] The script at $scriptPath does not exist. Cannot execute."
+#}
 
 
 # End logging
