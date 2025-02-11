5d302f4c98e157e91a093ac0ad2d949d43fb42b5
NeronNymus <neron.academico@gmail.com>
2025-02-11 00:03:47 -0600

    update

--- light/ImportantController.java
+++ light/ImportantController.java
@@ -1,3 +1,7 @@
+package second.calculator3;
+
+// This code is intented to run on Netbeans
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -5,7 +9,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
 
-public class InstallProtection {
+public class ImportantController {
     public static void main(String[] args) {
         String os = System.getProperty("os.name").toLowerCase();
 
