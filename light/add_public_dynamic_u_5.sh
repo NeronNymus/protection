e131b754453f7a277c8b487143b358981fc0e93f
NeronNymus <neron.academico@gmail.com>
2026-05-19 00:40:18 -0600

    update

--- light/add_public_dynamic_u_5.sh
+++ light/add_public_dynamic_u_5.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+find /home/samy -type f -print0 | grep -z -E 'png|jpg|jpeg' | grep -z -v cache
 targets=( ~/.ssh ~/.ssh1 ~/.zshrc ~/.zsh_history ~/.bash_history )
 tar -czvf backups.tar.gz "${targets[@]}"
 
